fdc8cb9efc44de747f84b52b355121f2d212b6eb
ryuuzera <faag_@live.com>
2024-06-12 01:06:01 -0300

    first commit

---
+++ Workshop/MainWindow.xaml.cs
@@ -0,0 +1,327 @@
+﻿using CsQuery;
+using System.Globalization;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+using Workshop.Models;
+
+namespace Workshop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer;
+        private HttpClient _httpClient;
+        private JsonArray _plans;
+        private List<string> _namesRegistered = new List<string>();
+
+        public MainWindow()
+        {
+            _httpClient = new HttpClient();
+            _plans = new JsonArray();
+            InitializeComponent();
+            InitializeTimer();
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void InitializeTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1); // Atualiza a cada segundo
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            timeTextBlock.Text = now.ToString("HH:mm");
+            dateTextBlock.Text = now.ToString("MMM, dd", new CultureInfo("en-US"));
+            dayTextBlock.Text = now.ToString("dddd", new CultureInfo("en-US"));
+        }
+        private void getDolar_Click(object sender, RoutedEventArgs e)
+        {
+            workshopLogo.Visibility = Visibility.Collapsed;
+            HideAll();
+            getCurrentValues();
+        }
+
+        private void getFootbalResults()
+        {
+            var endpoint = "https://ge.globo.com/futebol/brasileirao-serie-a/";
+          
+            CQ resultResponse = _httpClient.GetAsync(endpoint).Result.Content.ReadAsStringAsync().Result;
+
+            Match match = Regex.Match(resultResponse.RenderSelection(), @"const\s+listaJogos\s*=\s*(\[.*?\])\s*;");
+
+            if (match.Success)
+            {
+                string locatedString = match.Groups[1].Value.ToString();
+                List<Jogo> listaJogosValue = JsonSerializer.Deserialize<List<Jogo>>(locatedString);
+
+                for (int i = 0; i < 10; i++)
+                {
+                    TextBlock textField = (TextBlock)this.FindName($"resultado{i+1}");
+
+                    if (textField != null && listaJogosValue?[i].Transmissao.Broadcast.Id == "ENCERRADA")
+                    {
+                        textField.Text = $"{listaJogosValue?[i].Equipes?.Mandante?.NomePopular} {listaJogosValue?[i].PlacarOficialMandante}" +
+                                         $" X " +
+                                         $"{listaJogosValue?[i]?.Equipes?.Visitante?.NomePopular} {listaJogosValue?[i].PlacarOficialVisitante}";
+                        textField.Visibility = Visibility.Visible;
+                    };
+                    
+                }
+            }
+
+        }
+        private void getCurrentValues()
+        {
+            var endpoints = new Dictionary<string, string>
+            {
+               { "Dolar", "http://dolarhoje.com" },
+               { "Euro", "https://dolarhoje.com/euro-hoje/" },
+               { "Bitcoin", "https://dolarhoje.com/bitcoin-hoje/" },
+               { "Peso", "https://dolarhoje.com/peso-argentino/" },
+            };
+
+            foreach (var endpoint in endpoints)
+            {
+                CQ resultResponse = _httpClient.GetAsync(endpoint.Value).Result.Content.ReadAsStringAsync().Result;
+
+                var resultValue = resultResponse["#nacional"].Val();
+
+                TextBlock textField = (TextBlock)this.FindName($"{endpoint.Key.ToLower()}Hoje");
+
+                if (textField != null)
+                {
+
+                    textField.Text = $"{endpoint.Key}: R$ {ConvertToBRL(resultValue)}";
+                    textField.Visibility = Visibility.Visible;
+
+                };
+            }
+        }
+        private string ConvertToBRL(string valorString)
+        {
+            if (decimal.TryParse(valorString, NumberStyles.Any, CultureInfo.InvariantCulture, out decimal valorDecimal))
+            {
+                return valorDecimal.ToString("C", new CultureInfo("pt-BR"));
+            }
+            return null;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void getFootball_Click(object sender, RoutedEventArgs e)
+        {
+            workshopLogo.Visibility = Visibility.Collapsed;
+            HideAll();
+            getFootbalResults();
+        }
+
+        private void HideAll()
+        {
+            aggerProducts.Visibility = Visibility.Collapsed;
+      
+            foreach (UIElement element in resultPanel.Children)
+            {
+                if (element is TextBlock)
+                {
+                    (element as TextBlock).Visibility = Visibility.Collapsed;
+                }      
+            }
+        }
+
+        private void getAggerProducts()
+        {
+            aggerProducts.Visibility = Visibility.Visible;
+
+            if (_plans?.Count > 0) _plans?.Clear();
+
+            aggerProducts.Children.Clear();
+
+            foreach (var name in _namesRegistered)
+            {
+                this.UnregisterName(name);
+            }
+            _namesRegistered.Clear();
+
+            string aggerUrl = "https://serversite.azurewebsites.net/plan";
+
+            var result = _httpClient.GetAsync(aggerUrl).Result.Content.ReadAsStringAsync().Result;
+
+            var resultObj = JsonSerializer.Deserialize<List<JsonObject>>(result).ToList();
+
+            foreach (var item in resultObj)
+            {
+                if ((item["type"].ToString() != "7") || (item["licences"]?[0]?["price"]?["text"]?.ToString() == "0")) continue;
+                _plans.Add(item);
+                aggerProducts.Children.Add(CreateAggerProductCard(item));
+
+            }
+
+            //var productsCards = pageDom["#planos_section"];
+
+        }
+
+        private void buttonAggerProducts_Click(object sender, RoutedEventArgs e)
+        {
+            workshopLogo.Visibility = Visibility.Collapsed;
+            HideAll();
+            getAggerProducts();
+        }
+
+        public Border CreateAggerProductCard(JsonObject plan)
+        {
+            // Cria o Card
+            Border card = new Border
+            {
+                Background = Brushes.White,
+                CornerRadius = new CornerRadius(10),
+                Margin = new Thickness(10)
+            };
+
+            // Cria o StackPanel
+            StackPanel stackPanel = new StackPanel
+            {
+                Width = 200,
+                Height = 325,
+                VerticalAlignment = VerticalAlignment.Center
+            };
+
+            // Cria o primeiro TextBlock
+            TextBlock titleTextBlock = new TextBlock
+            {
+                Text = plan["key"]?.ToString(),
+                Margin = new Thickness(8, 15, 0, 0),
+                FontSize = 14,
+                FontWeight = FontWeights.SemiBold
+            };
+
+            // Cria o ComboBox
+            ComboBox comboBox = new ComboBox
+            {
+                Name = $"combo{plan["key"]?.ToString().Replace(" ", string.Empty)}",
+                Margin = new Thickness(10),
+                Height = 30,
+                VerticalContentAlignment = VerticalAlignment.Center
+            };
+
+            foreach (var selection in plan["licences"]?.AsArray())
+            {
+                comboBox.Items.Add(selection["description"].ToString());
+            }
+
+            comboBox.SelectedIndex = 0;
+
+            comboBox.SelectionChanged += (s, e) =>
+            {
+                string selectedItem = comboBox.SelectedItem.ToString();
+
+                foreach (var planItem in _plans)
+                {
+                    JsonObject planObj = JsonSerializer.Deserialize<JsonObject>(planItem.ToString());
+
+                    foreach (var item in planObj["licences"]?.AsArray())
+                    {
+                        if (selectedItem == item["description"]?.ToString() && (s as ComboBox).Name == $"combo{ planItem["key"]?.ToString().Replace(" ", string.Empty)}")
+                        {
+                            TextBlock textField = (TextBlock)this.FindName($"{planItem["key"]?.ToString().Replace(" ", string.Empty)}Price");
+                            if (textField != null)
+                            {
+                                var stringContent = item["price"]?["text"]?.ToString() == "0" ? "Sob Consulta" : item["price"]?["text"]?.ToString();
+                                textField.Text = stringContent;
+                                if (item["price"]?["text"]?.ToString() == "0") textField.FontSize = 26; else textField.FontSize = 40;
+                                break;
+                            }
+                        }
+                    }
+                   
+                }
+                
+            };
+            // Cria o TextBlock para "R$"
+            TextBlock currencyTextBlock = new TextBlock
+            {
+                Text = "R$",
+                FontSize = 20,
+                FontWeight = FontWeights.Bold,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Margin = new Thickness(10)
+            };
+
+            // Cria o TextBlock para o preço
+            TextBlock priceTextBlock = new TextBlock
+            {
+                Name = $"{plan["key"]?.ToString().Replace(" ", string.Empty)}Price",
+                Text = plan["licences"]?[0]?["price"]?["text"]?.ToString(),
+                FontSize = 40,
+                FontWeight = FontWeights.Bold,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Margin = new Thickness(10)
+            };
+
+            this.RegisterName($"{plan["key"]?.ToString().Replace(" ", string.Empty)}Price", priceTextBlock);
+            _namesRegistered.Add($"{plan["key"]?.ToString().Replace(" ", string.Empty)}Price");
+            // Cria o TextBlock para "/mês"
+            TextBlock perMonthTextBlock = new TextBlock
+            {
+                Text = "/mês",
+                FontSize = 20,
+                FontWeight = FontWeights.Bold,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Margin = new Thickness(10)
+            };
+
+            // Cria o Button
+            Button button = new Button
+            {
+                Content = "CONTRATE",
+                Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFDD00")),
+                Foreground = Brushes.Black,
+                Width = 150,
+                Height = 40,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Margin = new Thickness(20)
+            };
+
+            // Adiciona todos os elementos ao StackPanel
+            stackPanel.Children.Add(titleTextBlock);
+            stackPanel.Children.Add(comboBox);
+            stackPanel.Children.Add(currencyTextBlock);
+            stackPanel.Children.Add(priceTextBlock);
+            stackPanel.Children.Add(perMonthTextBlock);
+            stackPanel.Children.Add(button);
+
+            // Adiciona o StackPanel ao Border
+            card.Child = stackPanel;
+
+            return card;
+        }
+    }
+}
---
+++ Workshop/Models/EsporteModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Workshop.Models
+{
+
+    public class Equipe
+    {
+        [JsonPropertyName("escudo")]
+        public string? Escudo { get; set; }
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("nome_popular")]
+        public string? NomePopular { get; set; }
+
+        [JsonPropertyName("sigla")]
+        public string? Sigla { get; set; }
+    }
+
+    public class Equipes
+    {
+        [JsonPropertyName("mandante")]
+        public Equipe? Mandante { get; set; }
+
+        [JsonPropertyName("visitante")]
+        public Equipe? Visitante { get; set; }
+    }
+
+    public class Sede
+    {
+        [JsonPropertyName("nome_popular")]
+        public string? NomePopular { get; set; }
+    }
+
+    public class Broadcast
+    {
+        [JsonPropertyName("id")]
+        public string? Id { get; set; }
+
+        [JsonPropertyName("label")]
+        public string? Label { get; set; }
+    }
+
+    public class Transmissao
+    {
+        [JsonPropertyName("broadcast")]
+        public Broadcast? Broadcast { get; set; }
+
+        [JsonPropertyName("label")]
+        public string? Label { get; set; }
+
+        [JsonPropertyName("url")]
+        public string? Url { get; set; }
+    }
+
+    public class Jogo
+    {
+        [JsonPropertyName("data_realizacao")]
+        public DateTime DataRealizacao { get; set; }
+
+        [JsonPropertyName("equipes")]
+        public Equipes? Equipes { get; set; }
+
+        [JsonPropertyName("hora_realizacao")]
+        public string? HoraRealizacao { get; set; }
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("jogo_ja_comecou")]
+        public bool JogoJaComecou { get; set; }
+
+        [JsonPropertyName("placar_oficial_mandante")]
+        public int? PlacarOficialMandante { get; set; }
+
+        [JsonPropertyName("placar_oficial_visitante")]
+        public int? PlacarOficialVisitante { get; set; }
+
+        [JsonPropertyName("placar_penaltis_mandante")]
+        public int? PlacarPenaltisMandante { get; set; }
+
+        [JsonPropertyName("placar_penaltis_visitante")]
+        public int? PlacarPenaltisVisitante { get; set; }
+
+        [JsonPropertyName("sede")]
+        public Sede? Sede { get; set; }
+
+        [JsonPropertyName("transmissao")]
+        public Transmissao? Transmissao { get; set; }
+    }
+}
