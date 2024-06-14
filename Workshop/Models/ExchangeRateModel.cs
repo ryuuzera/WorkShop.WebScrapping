01436a378e4c0daabdc1381297eed6ac2d6d5d59
ryuuzera <faag_@live.com>
2024-06-14 02:31:03 -0300

    Refactor

--- Workshop/Models/ExchangeRateModel.cs
+++ Workshop/Models/ExchangeRateModel.cs
@@ -3,10 +3,10 @@ namespace Workshop.Models
 {
     public class ExchangeRateModel
     {
-        public string Name { get; set; }
-        public string StrangeValue { get; set; }
-        public string StrangeCoin { get; set; }
-        public string LocalValue { get; set; }
-        public string LocalCoin { get; set; }
+        public string? Name { get; set; }
+        public string? StrangeValue { get; set; }
+        public string? StrangeCoin { get; set; }
+        public string? LocalValue { get; set; }
+        public string? LocalCoin { get; set; }
     }
 }
